d6b931a38156023198d7a100bf2d1ab5182acae2
Alex van den Hoogen <alex3305@gmail.com>
2020-01-22 10:48:18 +0100

    Fixed possible missing ssl directory

--- traefik/run.sh
+++ traefik/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bashio
 
+bashio::log.debug "Ensuring SSL directory..."
+mkdir -p /ssl/traefik/
+
 bashio::log.info "Generating static config..."
 j2 /etc/traefik/traefik.yaml.j2 /data/options.json -o /etc/traefik/traefik.yaml
 bashio::log.info "Static config generated"
@@ -8,9 +11,9 @@ bashio::log.info "Extracting environment variables..."
 ENV_VARS=$(j2 /etc/traefik/env.j2 /data/options.json)
 
 if [ -z "$ENV_VARS" ]; then
-    bashio::log.info "No additional environment variables found"
+    bashio::log.debug "No additional environment variables found"
 else
-    bashio::log.info "Extracted variables ${ENV_VARS}"
+    bashio::log.debug "Extracted variables ${ENV_VARS}"
 fi
 
 bashio::log.info "Starting Traefik..."
